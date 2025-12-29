e933c13e99b53f9a2370e37d69e4798c15a67324
Nick S. <job-offers@nicks.guru>
2025-12-29 16:19:15 +0300

    refactor(core): changed Redis key

--- src/main/java/guru/nicks/commons/auth/domain/BlockedTokenHash.java
+++ src/main/java/guru/nicks/commons/auth/domain/BlockedTokenHash.java
@@ -1,20 +1,20 @@
 package guru.nicks.commons.auth.domain;
 
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.extern.jackson.Jacksonized;
 import org.springframework.data.annotation.Id;
-import org.springframework.data.keyvalue.annotation.KeySpace;
 import org.springframework.data.redis.core.RedisHash;
 import org.springframework.data.redis.core.TimeToLive;
 
 /**
- * Access token (presumably JWT) that has been blocked.
+ * Access token (presumably a JWT) that has been blocked.
  */
-@RedisHash
-@KeySpace("BlockedTokenHash")
+@RedisHash("blocked-access-token")
 //
 @Data
 @NoArgsConstructor
@@ -31,6 +31,8 @@ public class BlockedTokenHash {
      * Seconds until expiration.
      */
     @TimeToLive
-    private long timeToLiveSec;
+    @Min(0)
+    @NotNull
+    private Long timeToLiveSec;
 
 }
